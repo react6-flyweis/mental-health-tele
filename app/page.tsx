1b494a767172789305faaddbdbf706b67757d1ba
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-16 22:34:13 +0530

    feat: restructure Page component to include Hero, Support, Experts, and Getting Started sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function page() {
-  return <div>page</div>;
+import HeroSection from "@/components/HeroSection";
+import ExpertsSection from "@/components/ExpertsSection";
+import SupportSection from "@/components/SupportSection";
+import GettingStartedSection from "@/components/GettingStartedSection";
+
+export default function Page() {
+  return (
+    <main>
+      <HeroSection />
+      <SupportSection />
+      <ExpertsSection />
+      <GettingStartedSection />
+    </main>
+  );
 }
