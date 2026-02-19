5535fd5fee3fb9610e31f858193f3bafb49a287e
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-19 22:01:12 +0530

    feat: update image configuration to use remote patterns for better flexibility

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["randomuser.me"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+      },
+    ],
   },
 };
 
