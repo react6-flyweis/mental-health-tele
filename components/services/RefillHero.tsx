6b19cbcda823d9ce25f4cad6e920204f70a8d55c
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-19 21:55:21 +0530

    feat: add RefillHero component for prescription renewal section

---
+++ components/services/RefillHero.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { ArrowRight } from "lucide-react";
+import bgPattern from "@/assets/landing/hero/bg-pattern.png";
+import { Button } from "@/components/ui/button";
+
+export default function RefillHero() {
+  return (
+    <section className="p-10">
+      <div className="py-10 bg-accent rounded-2xl relative overflow-hidden">
+        <div className="absolute -top-5 -scale-x-100 z-0 opacity-50 max-w-xs">
+          <Image
+            src={bgPattern}
+            alt="Background pattern"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="absolute z-0 -top-5 right-0 opacity-50 max-w-xs">
+          <Image
+            src={bgPattern}
+            alt="Background pattern"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="container mx-auto px-4 md:px-6 relative z-10">
+          <div className="max-w-4xl mx-auto text-center">
+            <h1 className=" text-4xl md:text-5xl font-medium tracking-tight leading-tight">
+              <span className="block text-primary">Refill prescriptions</span>
+              <span className="block text-primary font-medium">
+                online,{" "}
+                <span className="text-slate-900 font-medium"> stress-free</span>
+              </span>
+            </h1>
+
+            <p className="mx-auto max-w-lg text-base md:text-lg text-muted-foreground leading-relaxed">
+              Request medication renewals digitally and continue your care with
+              confidence.
+            </p>
+          </div>
+
+          <div className="mt-10 flex justify-center">
+            <Button className="bg-gradient-primary" size="lg">
+              Schedule an online visit
+              <ArrowRight className="w-5 h-5" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
