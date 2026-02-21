8479da794f8455718125ddca296b8334bba3a620
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-21 18:39:19 +0530

    feat: create contact page with ContactHero, CompanyContactSection, and ContactSupportSection components

---
+++ app/(marketing)/(company)/contact/page.tsx
@@ -0,0 +1,13 @@
+import ContactHero from "@/components/company/ContactHero";
+import CompanyContactSection from "@/components/CompanyContactSection";
+import ContactSupportSection from "@/components/ContactSupportSection";
+
+export default function page() {
+  return (
+    <>
+      <ContactHero />
+      <CompanyContactSection />
+      <ContactSupportSection />
+    </>
+  );
+}
