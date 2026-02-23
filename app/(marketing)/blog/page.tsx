0dfeb49271c530e8872ed346e97b131ea63f1a4e
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-23 18:41:09 +0530

    feat: wrap BlogHero component in Suspense for improved loading state

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -2,13 +2,17 @@ import BlogHero from "@/components/blog/BlogHero";
 import ArticleGrid from "@/components/blog/ArticleGrid";
 import { articles } from "@/lib/blog";
 
+import { Suspense } from "react";
+
 export default function page() {
   // filter by tag from url if provided
   //   const activeTag = searchParams?.tag || "";
 
   return (
     <>
-      <BlogHero />
+      <Suspense fallback={<div className="h-20" />}>
+        <BlogHero />
+      </Suspense>
 
       <ArticleGrid articles={articles} />
     </>
