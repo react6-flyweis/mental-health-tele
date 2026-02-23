b8db58966db46720bed4b698ff7dd12d38a1513d
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-23 18:32:34 +0530

    feat: create editorial policy page with EditorialHero and EditorialPolicyContent components

---
+++ app/(marketing)/(footer)/editorial-policy/page.tsx
@@ -0,0 +1,11 @@
+import EditorialHero from "@/components/pages/EditorialHero";
+import EditorialPolicyContent from "@/components/pages/EditorialPolicyContent";
+
+export default function page() {
+  return (
+    <>
+      <EditorialHero />
+      <EditorialPolicyContent />
+    </>
+  );
+}
