97c7931bc933e0a160a051999ba3aeb1347f0c43
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-23 18:27:29 +0530

    feat: create FAQ page component to display FAQHero and FAQCategories

---
+++ app/(marketing)/faqs/page.tsx
@@ -0,0 +1,11 @@
+import FAQHero from "@/components/FAQHero";
+import FAQCategories from "@/components/FAQCategories";
+
+export default function page() {
+  return (
+    <>
+      <FAQHero />
+      <FAQCategories />
+    </>
+  );
+}
