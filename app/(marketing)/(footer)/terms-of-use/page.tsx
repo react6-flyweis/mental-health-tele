7be4d16fe4d51066d0b74ce3f369e54c2a6da54c
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-24 19:09:16 +0530

    feat: create Terms of Use page with TermsHero and TermsOfUseContent components

---
+++ app/(marketing)/(footer)/terms-of-use/page.tsx
@@ -0,0 +1,11 @@
+import TermsHero from "@/components/pages/TermsHero";
+import TermsOfUseContent from "@/components/pages/TermsOfUseContent";
+
+export default function page() {
+  return (
+    <div>
+      <TermsHero />
+      <TermsOfUseContent />
+    </div>
+  );
+}
