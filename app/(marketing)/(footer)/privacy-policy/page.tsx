52a0550aca8789041f679ebed2ee7021aa312f48
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-24 18:43:39 +0530

    feat: create PrivacyPolicyPage component to display privacy policy content

---
+++ app/(marketing)/(footer)/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import PrivacyPolicyContent from "@/components/pages/PrivacyPolicyContent";
+import PrivacyPolicyHero from "@/components/pages/PrivacyPolicyHero";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <>
+      <PrivacyPolicyHero />
+      <PrivacyPolicyContent />
+    </>
+  );
+}
