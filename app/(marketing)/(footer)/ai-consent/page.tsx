6a71bf29519605acc088d7472b045a3f45c30ce6
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-25 18:29:25 +0530

    feat: create AiConsent page with AiConsentHero and AiConsentContent components

---
+++ app/(marketing)/(footer)/ai-consent/page.tsx
@@ -0,0 +1,11 @@
+import AiConsentHero from "@/components/pages/AiConsentHero";
+import AiConsentContent from "@/components/pages/AiConsentContent";
+
+export default function page() {
+  return (
+    <div>
+      <AiConsentHero />
+      <AiConsentContent />
+    </div>
+  );
+}
