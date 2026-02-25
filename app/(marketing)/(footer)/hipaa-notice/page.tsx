7066f55518834401e83a1af23eb93d81b37b49fb
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-25 18:37:02 +0530

    feat: create HIPAANoticePage component to display HIPAA information

---
+++ app/(marketing)/(footer)/hipaa-notice/page.tsx
@@ -0,0 +1,11 @@
+import HIPAAHero from "@/components/pages/HIPAAHero";
+import HIPAANoticeContent from "@/components/pages/HIPAANoticeContent";
+
+export default function HIPAANoticePage() {
+  return (
+    <>
+      <HIPAAHero />
+      <HIPAANoticeContent />
+    </>
+  );
+}
