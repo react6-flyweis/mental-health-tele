180251cfefd4f442d88cfea6a2d3a8a36f9af2de
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-25 18:32:40 +0530

    feat: create PaymentTermsPage component to display payment terms information

---
+++ app/(marketing)/(footer)/payment-terms/page.tsx
@@ -0,0 +1,11 @@
+import PaymentTermsHero from "@/components/pages/PaymentTermsHero";
+import PaymentTermsSection from "@/components/pages/PaymentTermsSection";
+
+export default function PaymentTermsPage() {
+  return (
+    <>
+      <PaymentTermsHero />
+      <PaymentTermsSection />
+    </>
+  );
+}
