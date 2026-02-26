b2c334a408c3832345931adbe5268681a8009ce6
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-26 18:23:10 +0530

    feat: create HIPAAPrivacyPage to render HIPAA privacy policy sections

---
+++ app/(marketing)/(footer)/hipaa-privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import HIPAAPrivacyHero from "@/components/pages/HIPAAPrivacyHero";
+import HIPAAPrivacyContent from "@/components/pages/HIPAAPrivacyContent";
+
+export default function HIPAAPrivacyPage() {
+  return (
+    <>
+      <HIPAAPrivacyHero />
+      <HIPAAPrivacyContent />
+    </>
+  );
+}
