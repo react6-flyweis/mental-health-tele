5c5679e7c6dcf2536609b3537f7a6edd53e717f7
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-26 18:23:47 +0530

    feat: create SiteMapPage component to render sitemap with hero and content sections

---
+++ app/(marketing)/(footer)/sitemap/page.tsx
@@ -0,0 +1,11 @@
+import SitemapHero from "@/components/pages/SitemapHero";
+import SitemapContent from "@/components/pages/SitemapContent";
+
+export default function SiteMapPage() {
+  return (
+    <main>
+      <SitemapHero />
+      <SitemapContent />
+    </main>
+  );
+}
