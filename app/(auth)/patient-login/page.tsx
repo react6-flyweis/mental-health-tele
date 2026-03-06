a322160e8203fea553ab8664ed61fce6045f9548
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-03-06 17:50:15 +0530

    feat: add PatientLoginPage component for patient authentication

---
+++ app/(auth)/patient-login/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import LoginCard from "@/components/auth/LoginCard";
+
+export default function PatientLoginPage() {
+  return <LoginCard role="Patient" onSubmitPath="/dashboard" />;
+}
